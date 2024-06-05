9d84076088c668a0ed7398a5365a95fc174e4467
renka1225 <lotus1225r@gmail.com>
2024-06-05 16:50:56 +0900

    クォータニオン

---
+++ Test/Quaternion/main.cpp
@@ -0,0 +1,136 @@
+#include "DxLib.h"
+#include "Quaternion.h"
+#include <math.h>
+#include <memory>
+
+namespace
+{
+	constexpr float kCameraDist = 96;
+	constexpr float kCameraHeight = 32;
+	constexpr float kScale = 0.05f;
+}
+
+void DrawGrid()
+{
+	for (int x = -50; x <= 50; x += 10)
+	{
+		DrawLine3D(VGet(static_cast<float>(x), 0, -50), VGet(static_cast<float>(x), 0, 50), 0xffff00);
+	}
+	for (int z = -50; z <= 50; z += 10)
+	{
+		DrawLine3D(VGet(-50, 0, static_cast<float>(z)), VGet(50, 0, static_cast<float>(z)), 0xff0000);
+	}
+
+	// X+-,Z+-の方向が分かりやすいように表示を追加する
+	VECTOR dispPos = ConvWorldPosToScreenPos(VGet(50, 0, 0));
+	if (dispPos.z >= 0.0f && dispPos.z <= 1.0f)
+	{
+		DrawStringF(dispPos.x, dispPos.y, "X+", 0xffffff);
+	}
+
+	dispPos = ConvWorldPosToScreenPos(VGet(-50, 0, 0));
+	if (dispPos.z >= 0.0f && dispPos.z <= 1.0f)
+	{
+		DrawStringF(dispPos.x, dispPos.y, "X-", 0xffffff);
+	}
+
+	dispPos = ConvWorldPosToScreenPos(VGet(0, 0, 50));
+	if (dispPos.z >= 0.0f && dispPos.z <= 1.0f)
+	{
+		DrawStringF(dispPos.x, dispPos.y, "Z+", 0xffffff);
+	}
+
+	dispPos = ConvWorldPosToScreenPos(VGet(0, 0, -50));
+	if (dispPos.z >= 0.0f && dispPos.z <= 1.0f)
+	{
+		DrawStringF(dispPos.x, dispPos.y, "Z-", 0xffffff);
+	}
+}
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	// windowモード設定
+	ChangeWindowMode(true);
+
+	if (DxLib_Init() == -1)	// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+
+	SetUseZBuffer3D(true);
+	SetWriteZBuffer3D(true);
+	SetUseBackCulling(true);
+
+	// ダブルバッファモード
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	float cameraAngle = -DX_PI_F / 2;
+
+	/*Cube情報*/
+	int m_handle = MV1LoadModel("data/cube.mv1");
+	VECTOR m_pos = VGet(0.0f, 0.0f, 0.0f);
+	// 回転角度
+	float m_angle = 2 * DX_PI_F / 0.001f;
+	// 回転軸
+	VECTOR m_axis = VGet(1.0f, 1.0f, 1.0f);
+	// 単位行列に直す
+	VNorm(m_axis);
+
+	// 回転クォータニオン作成
+	Quaternion q {0.0f, 0.0f, 0.0f, 0.0f};
+	q.w = cos(m_angle / 2.0f);
+	q.x = m_axis.x * sin(m_angle / 2.0f);
+	q.y = m_axis.y * sin(m_angle / 2.0f);
+	q.z = m_axis.z * sin(m_angle / 2.0f);
+
+
+	MV1SetScale(m_handle, VGet(kScale, kScale, kScale));
+
+	while (ProcessMessage() == 0)
+	{
+		LONGLONG  time = GetNowHiPerformanceCount();
+		// 画面のクリア
+		ClearDrawScreen();
+
+		/*カメラ調整*/
+		if ((GetJoypadInputState(DX_INPUT_KEY_PAD1) & PAD_INPUT_5))
+		{
+			cameraAngle += 0.05f;
+		}
+		if ((GetJoypadInputState(DX_INPUT_KEY_PAD1) & PAD_INPUT_6))
+		{
+			cameraAngle -= 0.05f;
+		}
+
+		SetCameraNearFar(1.0f, 180.0f);
+		VECTOR cameraPos;
+		cameraPos.x = cosf(cameraAngle) * kCameraDist;
+		cameraPos.y = kCameraHeight;
+		cameraPos.z = sinf(cameraAngle) * kCameraDist;
+		SetCameraPositionAndTarget_UpVecY(cameraPos, VGet(0, 0, 0));
+
+		/*グリッド表示*/
+		DrawGrid();
+
+		// ゲームの処理
+		m_pos = q * m_pos;
+		MV1SetPosition(m_handle, m_pos);
+		MV1DrawModel(m_handle);
+
+		//裏画面を表画面を入れ替える
+		ScreenFlip();
+
+		// escキーを押したら終了する
+		if (CheckHitKey(KEY_INPUT_ESCAPE))	break;
+
+		// fpsを60に固定
+		while (GetNowHiPerformanceCount() - time < 16667)
+		{
+		}
+	}
+
+	DxLib_End();			// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
