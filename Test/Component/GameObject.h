65f3f72b31328bd0d8fe39595886984018de0ce6
renka1225 <lotus1225r@gmail.com>
2024-06-11 00:01:22 +0900

    up

--- Test/Component/GameObject.h
+++ Test/Component/GameObject.h
@@ -1,8 +1,23 @@
 #pragma once
+#include <list>
+#include <string>
 
 class GameObject
 {
 public:
 	template <typename T>
-	T GetComponent();
+	void AddComponent();
+	void GetComponent();
+	void DelComponent();
+	void ShowComponents();
+
+private:
+	std::list<GameObject> list;
+	//std::string objName = nullptr;
 };
+
+template<typename T>
+inline void GameObject::AddComponent()
+{
+	list.push_back(std::make_shared<T>(list));
+}
--- Test/Component/main.cpp
+++ Test/Component/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #include "GameObject.h"
+#include "UI.h"
+#include "Text.h"
+#include "Pos.h"
 
 int main()
 {
-	GameObject obj;
+	GameObject obj1;
+	GameObject obj2;
+
+	// AddComponent
+	obj1.AddComponent<UI>();
+	obj2.AddComponent<Text>();
+	obj2.AddComponent<Text>();
+
+	// GetComponent
+	UI temp1 = obj1.GetComponent<UI>();
+	temp1.ShowComponentName();
+	UI temp2 = obj2.GetComponent<UI>();
+	temp2.ShowComponentName();
+	UI temp3 = obj2.GetComponent<UI>();
+	temp3.ShowComponentName();
 
 	return 0;
 }
